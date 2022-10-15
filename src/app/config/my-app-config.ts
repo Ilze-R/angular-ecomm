9755aa79741273d94630cdff2b460938051ecccf
Ilze Rigerte <irigerte@gmail.com>
2022-10-15 15:29:04 +0300

    login with octa

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,8 @@
+export default {
+  oidc: {
+    clientId: '0oa6v327hkjEIhS815d7',
+    issuer: 'https://dev-87263724.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:4200/login/callback',
+    scopes: ['openid', 'profile', 'email'],
+  },
+};
